539d8724a3b204298d8325d676d7abeea6f4d9ff
JakubKotulski <jakub.kotulski@interia.com>
2022-10-26 13:15:16 +0200

    fix passport issue

--- node/passport-config.js
+++ node/passport-config.js
@@ -4,10 +4,8 @@ const localStrategy = require("passport-local").Strategy;
 
 const passportConfig = (passport) => {
     passport.use(
-        new localStrategy((email, password, done) => {
-            console.log("Jestem tu");
+        new localStrategy({ usernameField: 'email' }, (email, password, done) => {
             User.findOne({email: email}, (err, user) => {
-
                 if(err) throw err;
                 if(!user) return done(null, false);
                 bcrypt.compare(password, user.password, (err, result) => {
