6e5c5634ceb84070af779701d15c8a647e6dd103
JakubKotulski <jakub.kotulski@interia.com>
2022-10-26 12:30:24 +0200

    initial commit on branch

---
+++ node/passport-config.js
@@ -0,0 +1,42 @@
+const User = require("./models/user");
+const bcrypt = require("bcryptjs");
+const localStrategy = require("passport-local").Strategy;
+
+const passportConfig = (passport) => {
+    passport.use(
+        new localStrategy((email, password, done) => {
+            console.log("Jestem tu");
+            User.findOne({email: email}, (err, user) => {
+
+                if(err) throw err;
+                if(!user) return done(null, false);
+                bcrypt.compare(password, user.password, (err, result) => {
+                    if(err) throw err;
+                    if(result === true){
+                        return done(null, user);
+                    }else{
+                        return done(null, false)
+                    }
+                });
+            });
+        })
+    );
+
+    passport.serializeUser((user, cb) => {
+        cb(null, user.id)
+        
+    });
+    passport.deserializeUser((id, cb) => {
+        User.findOne({_id: id}, (err, user) => {
+            const userInformation = {
+                _id: user._id,
+                name: user.name,
+                surname: user.surname,
+                email: user.email
+            };
+            cb(err, userInformation);
+        });
+    });
+};
+
+module.exports = passportConfig;
