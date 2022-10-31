675e377ca044a3e1b7e8c1029a31ea626e2404f4
JakubKotulski <jakub.kotulski@interia.com>
2022-10-31 11:15:48 +0100

    financial authorization to play roulette

--- node/actions/getLogedUser.js
+++ node/actions/getLogedUser.js
@@ -1,6 +1,17 @@
-const getLogedUser = (req, res) => {
-  console.log(req.user)
-  res.json(req.user);
+const AccountBalance = require("../models/accountBalance");
+
+const getLogedUser = async (req, res) => {
+  try {
+    const balance = await AccountBalance.findOne({ userID: req.user._id });
+    const userInfo = {
+      user: req.user,
+      balance: balance.state,
+    };
+    res.status(200);
+    res.json(userInfo);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = { getLogedUser };
--- node/actions/playRoulette.js
+++ node/actions/playRoulette.js
@@ -3,28 +3,32 @@ const AccountBalance = require("../models/accountBalance");
 const playRoulette = async (req, res) => {
   try {
     const bet = parseInt(req.body.credits);
-
-    const result = Math.floor(Math.random() * 21);
-
-    if (result === parseInt(req.body.number)) {
+    const actuallBalance = await AccountBalance.findOne({ userID: req.body.id });
+    if (actuallBalance.state < bet) {
       const toSend = {
-        message: "You have won your bet!",
-        result: result,
+        message: "You don't have enough credits!",
+        result: "",
       };
-      const newBalance = bet * 30;
-      await AccountBalance.updateOne({ userID: req.body.id }, { state: newBalance });
-
       res.send(toSend);
     } else {
-      const toSend = {
-        message: "You have lost your bet!",
-        result: result,
-      };
-      const actuallBalance = await AccountBalance.findOne({ userID: req.body.id });
-      const newBalance = actuallBalance.state - bet;
-      await AccountBalance.updateOne({ userID: req.body.id }, { state: newBalance });
-
-      res.send(toSend);
+      const result = Math.floor(Math.random() * 21);
+      if (result === parseInt(req.body.number)) {
+        const toSend = {
+          message: "You have won your bet!",
+          result: result,
+        };
+        const newBalance = bet * 30 + actuallBalance;
+        await AccountBalance.updateOne({ userID: req.body.id }, { $set: { state: newBalance } });
+        res.send(toSend);
+      } else {
+        const toSend = {
+          message: "You have lost your bet!",
+          result: result,
+        };
+        const newBalance = actuallBalance.state - bet;
+        await AccountBalance.updateOne({ userID: req.body.id }, { state: newBalance });
+        res.send(toSend);
+      }
     }
   } catch (err) {
     console.log(err);
