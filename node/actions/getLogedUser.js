7c22ba4bedacd3235d3b3df1b19da1b63037234b
JakubKotulski <jakub.kotulski@interia.com>
2022-11-02 09:32:25 +0100

    fix issues

--- node/actions/getLogedUser.js
+++ node/actions/getLogedUser.js
@@ -3,12 +3,8 @@ const AccountBalance = require("../models/accountBalance");
 const getLogedUser = async (req, res) => {
   try {
     const balance = await AccountBalance.findOne({ userID: req.user._id });
-    const userInfo = {
-      user: req.user,
-      balance: balance.state,
-    };
     res.status(200);
-    res.json(userInfo);
+    res.json(balance);
   } catch (err) {
     console.log(err);
   }
