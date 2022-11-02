6cf7289883efa0864dc97eddb4f589a157d11d72
JakubKotulski <jakub.kotulski@interia.com>
2022-11-02 09:08:04 +0100

    clean up code

--- node/actions/playRoulette.js
+++ node/actions/playRoulette.js
@@ -10,26 +10,27 @@ const playRoulette = async (req, res) => {
         result: "",
       };
       res.send(toSend);
-    } else {
-      const result = Math.floor(Math.random() * 21);
-      if (result === parseInt(req.body.number)) {
-        const toSend = {
-          message: "You have won your bet!",
-          result: result,
-        };
-        const newBalance = bet * 30 + actuallBalance;
-        await AccountBalance.updateOne({ userID: req.body.id }, { $set: { state: newBalance } });
-        res.send(toSend);
-      } else {
-        const toSend = {
-          message: "You have lost your bet!",
-          result: result,
-        };
-        const newBalance = actuallBalance.state - bet;
-        await AccountBalance.updateOne({ userID: req.body.id }, { state: newBalance });
-        res.send(toSend);
-      }
+      return;
     }
+    const result = Math.floor(Math.random() * 21);
+    if (result === parseInt(req.body.number)) {
+      const toSend = {
+        message: "You have won your bet!",
+        result: result,
+      };
+      const newBalance = bet * 30 + actuallBalance.state;
+      await AccountBalance.updateOne({ userID: req.body.id }, { $set: { state: newBalance } });
+      res.send(toSend);
+      return;
+    }
+    const toSend = {
+      message: "You have lost your bet!",
+      result: result,
+    };
+    const newBalance = actuallBalance.state - bet;
+    await AccountBalance.updateOne({ userID: req.body.id }, { state: newBalance });
+    res.send(toSend);
+    return;
   } catch (err) {
     console.log(err);
   }
