7524df2344334a4f2c76d30f5b8218b92aa10cf6
Marcin <marcin.mika123@gmail.com>
2022-11-04 12:03:22 +0100

    message changed

--- node/actions/login.js
+++ node/actions/login.js
@@ -3,7 +3,7 @@ const passport = require("passport");
 const login = (req, res, next) => {
   passport.authenticate("local", (err, user, info) => {
     if (err) throw err;
-    if (!user) res.json("No User Exist");
+    if (!user) res.json("The Email address or password is inccorect");
     else {
       req.logIn(user, (err) => {
         if (err) throw err;
